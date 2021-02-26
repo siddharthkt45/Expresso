f7110b3c08fc3b27dd316c4132db450f30d633cd
siddharthkt45 <tiwarisk1471@gmail.com>
2021-02-26 06:24:44 +0530

    modified ExpressoClient and ExpressoClientTests

--- api/src/main/java/io/expresso/api/ExpressoClient.kt
+++ api/src/main/java/io/expresso/api/ExpressoClient.kt
@@ -1,14 +1,41 @@
 package io.expresso.api
 
 import io.expresso.api.services.ExpressoAPI
+import io.expresso.api.services.ExpressoAuthAPI
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 class ExpressoClient {
-    val retrofit: Retrofit = Retrofit.Builder()
+    val authToken: String? = null
+
+    private val authInterceptor = Interceptor { chain ->  
+        var req = chain.request()
+        authToken?.let {
+            req = req.newBuilder()
+                        .header("Authorization", "Token $it")
+                        .build()
+        }
+        chain.proceed(req)
+    }
+
+    val okHttpBuilder = OkHttpClient.Builder()
+                                    .readTimeout(5, TimeUnit.SECONDS)
+                                    .connectTimeout(2, TimeUnit.SECONDS)
+
+    val retrofitBuilder = Retrofit.Builder()
                                     .baseUrl("https://conduit.productionready.io/api/")
                                     .addConverterFactory(MoshiConverterFactory.create())
-                                    .build()
 
-    val api = retrofit.create(ExpressoAPI::class.java)
+    val publicApi = retrofitBuilder
+                    .client(okHttpBuilder.build())
+                    .build()
+                    .create(ExpressoAPI::class.java)
+
+    val authApi = retrofitBuilder
+                    .client(okHttpBuilder.addInterceptor(authInterceptor).build())
+                    .build()
+                    .create(ExpressoAuthAPI::class.java)
 }
--- api/src/test/java/io/expresso/api/ExpressoClientTests.kt
+++ api/src/test/java/io/expresso/api/ExpressoClientTests.kt
@@ -1,7 +1,12 @@
 package io.expresso.api
 
+import io.expresso.api.models.entities.SignupData
+import io.expresso.api.models.requests.SignupRequest
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Test
+import kotlin.random.Random
 
 class ExpressoClientTests {
 
@@ -9,7 +14,39 @@ class ExpressoClientTests {
 
     @Test
     fun `GET articles`() {
-        val articles = expressoClient.api.getArticles().execute()
-        assertNotNull(articles.body()?.articles)
+        runBlocking {
+            val articles = expressoClient.publicApi.getArticles()
+            assertNotNull(articles.body()?.articles)
+        }
+    }
+
+    @Test
+    fun `GET articles by author`() {
+        runBlocking {
+            val articles = expressoClient.publicApi.getArticles(author = "444")
+            assertNotNull(articles.body()?.articles)
+        }
+    }
+
+    @Test
+    fun `GET articles by tags`() {
+        runBlocking {
+            val articles = expressoClient.publicApi.getArticles(tag = "dragons")
+            assertNotNull(articles.body()?.articles)
+        }
+    }
+
+    @Test
+    fun `POST users - create user`() {
+        val userCreds = SignupData(
+            email = "testemail${Random.nextInt(999, 9999)}@test.com",
+            password = "pass${Random.nextInt(9999, 999999)}",
+            username = "rand_user_${Random.nextInt(99, 999)}"
+        )
+
+        runBlocking {
+            val resp = expressoClient.publicApi.signupUser(SignupRequest(userCreds))
+            assertEquals(userCreds.username, resp.body()?.user?.username)
+        }
     }
 }
